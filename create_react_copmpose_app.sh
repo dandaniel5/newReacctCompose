af91807195ed75736f5ab1900359381333d56365
Danil Kodolov <88844682+dandaniel5@users.noreply.github.com>
2023-05-26 04:20:10 +0400

    Update create_react_copmpose_app.sh
    add web friendly suggestion name,
    add default values, now you can spam enter and use js npm and this folder web friendly name

--- create_react_copmpose_app.sh
+++ create_react_copmpose_app.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Function to check if a string is web-friendly
+is_web_friendly() {
+  # Regular expression to match web-friendly names (lowercase letters, numbers, and hyphens)
+  local pattern="^[a-z0-9-]+$"
+  if [[ $1 =~ $pattern ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# Function to convert a string to web-friendly format
+to_web_friendly() {
+  local input=$1
+  local web_friendly=$(echo "$input" | tr '[:upper:]' '[:lower:]' | sed -e 's/[^a-z0-9]/-/g' -e 's/--/-/g')
+  echo "$web_friendly"
+}
+
 # Prompt for app name
 read -p "Enter the name for your React app (leave blank to use current directory name): " app_name
 
@@ -7,18 +25,37 @@ if [[ -z "${app_name// }" ]]; then
   app_name=${PWD##*/}
 fi
 
+# Check if app name is web-friendly
+while ! is_web_friendly "$app_name"; do
+  echo "Invalid app name. App name should only contain lowercase letters, numbers, and hyphens."
+  web_friendly_name=$(to_web_friendly "$app_name")
+  echo "Web-friendly alternative: $web_friendly_name"
+  read -p "Enter a web-friendly name for your React app (or press Enter to use the web-friendly alternative): " new_app_name
+  if [[ -z "${new_app_name// }" ]]; then
+    app_name=$web_friendly_name
+  else
+    app_name=$new_app_name
+  fi
+done
+
 # Prompt for package manager
-read -p "Enter the package manager you want to use (npm/yarn): " package_manager
+read -p "Enter the package manager you want to use (npm/yarn) [default: npm]: " package_manager
+
+package_manager=${package_manager:-npm}  # Set default value if input is empty
 
 while [[ "$package_manager" != "npm" && "$package_manager" != "yarn" ]]; do
-  read -p "Invalid input. Enter the package manager you want to use (npm/yarn): " package_manager
+  read -p "Invalid input. Enter the package manager you want to use (npm/yarn) [default: npm]: " package_manager
+  package_manager=${package_manager:-npm}  # Set default value if input is empty
 done
 
 # Prompt for language
-read -p "Enter the language you want to use (ts/js): " language
+read -p "Enter the language you want to use (ts/js) [default: js]: " language
+
+language=${language:-js}  # Set default value if input is empty
 
 while [[ "$language" != "ts" && "$language" != "js" ]]; do
-  read -p "Invalid input. Enter the language you want to use (ts/js): " language
+  read -p "Invalid input. Enter the language you want to use (ts/js) [default: js]: " language
+  language=${language:-js}  # Set default value if input is empty
 done
 
 # Create React app
@@ -52,12 +89,12 @@ WORKDIR /$app_name
 
 EXPOSE 3000
 
-COPY start.sh /start.sh
+COPY reactStart.sh /reactStart.sh
 
-RUN chmod +x /start.sh
+RUN chmod +x /reactStart.sh
 
 
-CMD ["/start.sh"]
+CMD ["/reactStart.sh"]
 
 
 EOL
@@ -74,8 +111,8 @@ services:
       - ./$app_name:/$app_name
 EOL
 
-# Create start.sh script
-cat > start.sh << EOL
+# Create reactStart.sh script
+cat > reactStart.sh << EOL
 #!/bin/ash
 
 if [ "$package_manager" == "npm" ]; then
@@ -89,7 +126,7 @@ else
 fi
 EOL
 
-chmod +x start.sh
+chmod +x reactStart.sh
 
 echo "React app '$app_name' created successfully with $package_manager and $language!"
 echo "To start the app, run 'docker-compose up'"
